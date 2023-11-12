e8519753f1ea4b1954ee1e9238c8c9404a74a298
Klombyls <kolya.vasiliev.louhi@gmail.com>
2023-11-12 10:41:03 +0300

    main

--- cameraTest.py
+++ cameraTest.py
@@ -12,35 +12,8 @@ def drawRectangle(img, polygons):
     for i in reversed(range(len(polygons))):
         cv2.line(img, polygons[i], polygons[i - 1], color, thickness)
 
-def main():
-    checker = DangerZoneHandler(modelName)
-    checker.loadDangerZone(dangerZonesPath)
-    # получаем видео с камеры
-    video=cv2.VideoCapture(0)
-    # пока не нажата любая клавиша — выполняем цикл
-    while cv2.waitKey(1)<0:
-        # получаем очередной кадр с камеры
-        hasFrame,frame=video.read()
-        # если кадра нет
-        if not hasFrame:
-            # останавливаемся и выходим из цикла
-            cv2.waitKey()
-            break
-        # выводим картинку с камеры
-        response = model(frame)
-        for recognizedObjects in response:
-            js = json.loads(recognizedObjects.tojson())
-            for object in js:
-                points = [
-                    (int(object['box']['x1']), int(object['box']['y1'])),
-                    (int(object['box']['x1']), int(object['box']['y2'])),
-                    (int(object['box']['x2']), int(object['box']['y2'])),
-                    (int(object['box']['x2']), int(object['box']['y1']))
-                ]
-                drawRectangle(frame, points)
-        cv2.imshow("Face detection", frame)
 
-def test():
+def main():
     checker = DangerZoneHandler(modelName)
     checker.loadDangerZone(dangerZonesPath)
     # получаем видео с камеры
@@ -60,4 +33,4 @@ def test():
         cv2.imshow("Face detection", result[-1])
 
 if __name__ == '__main__':
-    test()
+    main()
